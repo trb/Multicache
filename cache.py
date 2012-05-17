5a5cfbf90c4f6324cdfe9c55af6602e921f77076
Thomas <thomas.rubbert@gmail.com>
2012-05-16 23:15:12 -0700

    Fixed some bugs, added test coverage for more use cases

--- cache.py
+++ cache.py
@@ -48,6 +48,7 @@ def cache_function(function_or_key):
 
 __register = []
 __open_queue = False
+__in_init = False
 __cache = {}
 __next_provider = None
 __update = []
@@ -57,7 +58,8 @@ def __register_update(id_, values):
     __update.append((id_, values))
 
 
-def __do_updates():
+def do_updates():
+    global __update
     for id_, values in __update:
         __storage.set(id_, values)
 
@@ -72,7 +74,9 @@ def __do_queue():
     __open_queue = False
 
     for id_, self, provider in __register:
-        __storage.set(id_, provider())
+        if not __storage.has(id_):
+            __storage.set(id_, provider(self))
+
         self.__cached__ = __storage.get(id_)
 
     __register = []
@@ -89,11 +93,24 @@ def __make_id(cls, self, id_attribute):
     return 'class:' + cls.__name__ + str(self.__dict__[id_attribute])
 
 
-""" Cachable attributes don't have to be specified since
-self.__cached__.keys() will provide all attributes that were
-retrieved from cache (and could subsequently be updated).
-"""
+def __should_do_queue(self):
+    if not __open_queue:
+        return False
+
+    if '__in_init' in self.__dict__:
+        if self.__dict__['__in_init']:
+            return False
+    else:
+        return False
+
+    return True
+
+
 def cache_class(id_attribute):
+    """Cachable attributes don't have to be specified since
+    self.__cached__.keys() will provide all attributes that were
+    retrieved from cache (and could subsequently be updated).
+    """
     def make_class(cls):
         global __next_provider
         if __next_provider is None:
@@ -102,49 +119,45 @@ def cache_class(id_attribute):
                               + " function that returns data for the"
                               + " instance")
         provider_function = __next_provider
-
         __next_provider = None
 
         old_init = cls.__init__
         def new_init(self, *args, **kwargs):
+            self.__in_init = True
             old_init(self, *args, **kwargs)
-
-            def provider_wrapped():
-                return provider_function(self)
+            self.__in_init = False
 
             __register_class(__make_id(cls, self, id_attribute),
-                             self, provider_wrapped)
-
+                             self, provider_function)
         cls.__init__ = new_init
 
         old_getattribute = cls.__getattribute__
         def new_getattribute(self, key):
-            if __open_queue and key != '__dict__' and key != '__cached__':
-                __do_queue()
+            if key != '__dict__' and key != '__cached__':
+                if __should_do_queue(self):
+                    __do_queue()
 
-            if key != '__cached__' and key != '__dict__'\
-                and '__cached__' in self.__dict__\
-                and key in self.__cached__:
-
-                return self.__cached__[key]
+                if hasattr(self, '__cached__') and key in self.__cached__:
+                    return self.__cached__[key]
 
             return old_getattribute(self, key)
         cls.__getattribute__ = new_getattribute
 
         old_setattr = cls.__setattr__
         def new_setattr(self, key, value):
-            if __open_queue:
-                __do_queue()
+            if key != '__cache__':
+                if __should_do_queue(self):
+                    __do_queue()
 
-            if '__cached__' in self.__dict__:
+            if hasattr(self, '__cached__'):
                 """Only check for updatable cache values
                 when a cache dict exists"""
                 if not hasattr(self, '__cachable_attrs'):
                     self.__dict__['__cachable_attrs'] = \
-                                        self.__cached__.keys()
+                                        self.__dict__['__cached__'].keys()
 
-                if key in self.__cachable_attrs:
-                    if key != self.__cached__[key]:
+                if key in self.__dict__['__cachable_attrs']:
+                    if key != self.__dict__['__cached__'][key]:
                         self.__dict__['__cached__'][key] = value
                         __register_update(
                               __make_id(cls, self, id_attribute),
@@ -152,13 +165,23 @@ def cache_class(id_attribute):
                         return
 
             old_setattr(self, key, value)
-
         cls.__setattr__ = new_setattr
 
+        def hasattr(self, key):
+            if __should_do_queue(self):
+                __do_queue()
+
+            if '__cache__' in self.__dict__:
+                if key in self.__dict__['__cache__']:
+                    return True
+
+            if key in self.__dict__:
+                return True
+
+            return False
+        cls.__hasattr__ = hasattr
+
         return cls
-
-
-
     return make_class
 
 
--- tests/test_class.py
+++ tests/test_class.py
@@ -1,10 +1,22 @@
-from ..cache import cache_class, cache_provider, set_storage
+from ..cache import cache_class, cache_provider, set_storage, do_updates
 from ..backends.Local import LocalBackend
 from ..backends import Cache
 from nose.tools import with_setup
 
 
 _provider_count = 0
+
+
+class MockBackend(Cache):
+    def __init__(self, Backend):
+        super(MockBackend, self).__init__(Backend)
+
+    def get_storage(self):
+        return self.backend._storage
+
+
+storage = MockBackend(LocalBackend())
+set_storage(storage)
 
 
 @cache_class(id_attribute='user_id')
@@ -22,7 +34,10 @@ class C(object):
 
 
 def setup():
-    set_storage(Cache(LocalBackend()))
+    global storage
+    storage = MockBackend(LocalBackend())
+    set_storage(storage)
+
 
 def teardown():
     global _provider_count
@@ -69,5 +84,63 @@ def test_provider():
     assert _provider_count == 0, 'Instantiating a second class should not retrieve data'
 
     c1.name # Access data, execute pipeline
-
     assert _provider_count == 2, 'Two objects from pipeline should be loaded'
+
+
+@with_setup(setup, teardown)
+def test_write():
+    """Make sure that cache gets updated after cachable attributes are set"""
+    import json
+
+    c1 = C(1)
+
+    c1.name = 'Testname'
+    do_updates()
+    s = storage.get_storage()['class:C1'][1]
+    s = json.loads(s)
+    assert s['name'] == 'Testname'
+
+
+@with_setup(setup, teardown)
+def test_has():
+    """Check that hasattr works with auto-load attributes"""
+    c1 = C(1)
+    assert hasattr(c1, 'name'), 'This should be autoloaded when checking existence'
+    assert _provider_count == 1, 'Provider should have been called'
+
+
+@with_setup(setup, teardown)
+def test_multi_write():
+    """Make sure that writes into multiple classes don't interfere with
+    each other"""
+    import json
+
+    c1 = C(1)
+    c2 = C(2)
+
+    c1.name = 'Testname one'
+    c2.name = 'Testname two'
+
+    do_updates()
+
+    s = storage.get_storage()['class:C1'][1]
+    s = json.loads(s)
+    assert s['name'] == 'Testname one', 'Wrong name stored for first class'
+
+    s = storage.get_storage()['class:C2'][1]
+    s = json.loads(s)
+    assert s['name'] == 'Testname two', 'Wrong name stored for second class'
+
+
+@with_setup(setup, teardown)
+def test_cache_read():
+    """Make sure that provider isn't called if there's a cache hit, and the
+    changed value in cache is respected"""
+    c1 = C(1)
+
+    c1.name = 'Some name'
+    do_updates()
+
+    c_test = C(1)
+    assert c_test.name == 'Some name', 'Name is wrong in second instance'
+    assert _provider_count == 1, 'Provider should only have been called once (second instance should be a cache hit)'
